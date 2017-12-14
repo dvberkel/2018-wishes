6e46b609879516304e433fa1776b2c2543088688
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-14 12:48:42 +0100

    Compile template and output it to console

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+const Handlebars = require('handlebars');
+const fs = require('fs');
+
+const template_content = fs.readFileSync('template/story.handlebars');
+const template = Handlebars.compile(template_content.toString());
+
+var result = template({
+    story: "<p>You find yourself in a dark room.</p>"
+});
+console.log(result);
