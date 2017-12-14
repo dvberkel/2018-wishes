0eccdb0f7e9ab82a53273d326d84a60e292cf356
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-14 13:44:16 +0100

    Use index.js to create the documents

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-var template = require('./lib/template');
-var md = require('markdown').markdown;
+const fs = require('fs');
+const path = require('path');
+const md = require('markdown').markdown;
 
-var result = template.story({
-    story: md.toHTML('you find yourself in a *dark* room.')
+const template = require('./lib/template');
+
+const content_path = 'content';
+const docs_path = 'docs';
+fs.readdir(content_path, function(error, files){
+    if (error) { throw error; }
+    files.forEach(function(file){
+        const source_path = path.join(content_path, file);
+        const source = fs.readFileSync(source_path, { encoding: 'utf-8' });
+
+        const [story, links] = source.split('---');
+
+        const content = template.story({
+            story: md.toHTML(story)
+        });
+        const output_file = file.replace('story', 'html');
+        const output_path = path.join(docs_path, output_file);
+        fs.writeFileSync(output_path, content);
+    });
 });
-console.log(result);
