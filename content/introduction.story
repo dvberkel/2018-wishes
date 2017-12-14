70b971aa94857d7173629a11bfe27e2b52d5b551
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-14 14:54:46 +0100

    Transform links into directions

--- content/introduction.story
+++ content/introduction.story
@@ -1,5 +1,6 @@
 You find yourself in a dark room. What do you do?
 
 ---
+
 * [Find a light switch.](switch)
 * [Go back to sleep.](sleep)
--- index.js
+++ index.js
@@ -14,11 +14,35 @@ fs.readdir(content_path, function(error, files){
 
         const [story, links] = source.split('---');
 
+        var directions = undefined;
+        if (links) {
+            const tree = md.parse(links);
+            transform_links(tree);
+            directions = md.renderJsonML(md.toHTMLTree(tree));
+        }
+
         const content = template.story({
-            story: md.toHTML(story)
+            story: md.toHTML(story),
+            directions: directions
         });
         const output_file = file.replace('story', 'html');
         const output_path = path.join(docs_path, output_file);
         fs.writeFileSync(output_path, content);
     });
 });
+
+function transform_links(jsonml) {
+    if (jsonml[0] === 'bulletlist') {
+        const items = jsonml.slice(1);
+        items.forEach(transform_links);
+    }
+    else if (jsonml[0] === 'link') {
+        jsonml[1].href = jsonml[1].href + '.html';
+    }
+    else if (Array.isArray(jsonml[1])) {
+        transform_links(jsonml[1]);
+    }
+    else {
+        /* do nothing */
+    }
+}
