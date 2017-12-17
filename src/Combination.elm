d3c1b252a3a046b2e3da213127a811cc602c1c67
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-17 10:41:43 +0100

    Show current

--- src/Combination.elm
+++ src/Combination.elm
@@ -1,20 +1,22 @@
 module Combination exposing (..)
 
 import Html
+import Html.Attributes as Attribute
 
 
 main =
-    Html.program {
-            init = init 37
-                , update = update
-                , view= view
-                , subscriptions= subscriptions
+    Html.program
+        { init = init 37
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
         }
 
 
-init: Int -> (Model, Cmd msg)
+init : Int -> ( Model, Cmd msg )
 init target =
-    ({ current= 0, target= target}, Cmd.none)
+    ( { current = 0, target = target }, Cmd.none )
+
 
 type alias Model =
     { current : Int
@@ -27,14 +29,41 @@ type Message
     | Decrease Int
 
 
-update : Message -> Model -> (Model, Cmd msg)
+update : Message -> Model -> ( Model, Cmd msg )
 update message model =
-    (model, Cmd.none)
+    ( model, Cmd.none )
 
 
 view : Model -> Html.Html msg
 view model =
-    Html.text "Hello, World!"
+    let
+        ds =
+            (digits 3 model.current)
+                |> List.map toString
+                |> List.map Html.text
+    in
+        Html.div [ Attribute.class "combination-lock" ]
+            ds
+
+
+digits : Int -> Int -> List Int
+digits levels n =
+    List.reverse (reversed_digits levels n)
+
+
+reversed_digits : Int -> Int -> List Int
+reversed_digits levels n =
+    if levels == 0 then
+        []
+    else
+        let
+            suffix =
+                digits (levels - 1) (n // 10)
+
+            digit =
+                n % 10
+        in
+            digit :: suffix
 
 
 subscriptions : Model -> Sub msg
