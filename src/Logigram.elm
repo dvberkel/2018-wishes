01cf7f09574689fd666f69275f9cb23258b1c9a2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-19 08:42:41 +0100

    Create a logigram model

--- src/Logigram.elm
+++ src/Logigram.elm
@@ -5,3 +5,18 @@ import Html
 
 main =
     Html.text "Hello, World"
+
+
+type Family
+    = Daan
+    | Marlies
+    | Sophie
+    | Robin
+    | Hannah
+
+type Hat
+    = Red
+    | Orange
+    | Yellow
+    | Green
+    | Blue
