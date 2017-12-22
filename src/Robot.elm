1562c04ed22eeecaf78fdca380f34033ab32a2ee
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-22 10:48:49 +0100

    Model a robot

--- src/Robot.elm
+++ src/Robot.elm
@@ -5,3 +5,30 @@ import Html
 
 main =
     Html.text "Hello, World!"
+
+
+type alias Robot =
+    { heading : Heading
+    , position : Position
+    }
+
+
+type Heading
+    = North
+    | East
+    | South
+    | West
+
+
+type alias Position =
+    ( Int, Int )
+
+
+type Program
+    = Idle
+    | Move
+    | Left
+    | Right
+    | Sequence (List Program)
+    | Repeat Int Program
+
