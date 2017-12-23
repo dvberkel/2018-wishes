ff0f8de05e2d5f83eb4f89425e3a4c06e5831f1c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-23 14:14:34 +0100

    Embed RobotPuzzle

--- content/fire.story
+++ content/fire.story
@@ -1,1 +1,8 @@
 Fire door
+
+---
+
+---
+
+<script src="js/RobotPuzzle.js"></script>
+<script src="js/robot-app.js"></script>
---
+++ docs/js/robot-app.js
@@ -0,0 +1,4 @@
+(function(document, Elm){
+    var container = document.getElementById('container');
+    Elm.RobotPuzzle.embed(container);
+})(document, Elm);
