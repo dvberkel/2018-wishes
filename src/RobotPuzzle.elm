3feee4f970f765daa9353483142997c7c25f2876
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-12-23 13:32:33 +0100

    Create a representation

--- src/RobotPuzzle.elm
+++ src/RobotPuzzle.elm
@@ -1,6 +1,7 @@
 module RobotPuzzle exposing (..)
 
 import Html
+import Html.Attributes as Attribute
 import Html.Events as Event
 import Robot exposing (..)
 
@@ -16,7 +17,7 @@ main =
 
 init : Robot.Program -> ( Model, Cmd Message )
 init program =
-    ( { state = load (1,1) program
+    ( { state = load ( 1, 1 ) program
       , world = """################################################
 #                                              #
 #                                              #
@@ -60,7 +61,72 @@ view model =
     Html.div []
         [ Html.button [ Event.onClick Step ] [ Html.text ">" ]
         , Html.span [] [ Html.text (toString model.state) ]
+        , viewWorld model
         ]
+
+
+viewWorld : Model -> Html.Html Message
+viewWorld model =
+    let
+        world =
+            model.world
+
+        ( _, max_y ) =
+            world.bounding_box
+
+        ys =
+            List.range 0 max_y
+
+        ds =
+            List.map (viewWorldRow model) ys
+    in
+        Html.div [ Attribute.class "world" ] ds
+
+
+viewWorldRow : Model -> Int -> Html.Html Message
+viewWorldRow model y =
+    let
+        world =
+            model.world
+
+        ( max_x, _ ) =
+            world.bounding_box
+
+        xs =
+            List.range 0 max_x
+
+        ds =
+            List.map (viewWorldPosition model y) xs
+    in
+        Html.div [ Attribute.class "row" ] ds
+
+
+viewWorldPosition : Model -> Int -> Int -> Html.Html Message
+viewWorldPosition model y x =
+    let
+        world =
+            model.world
+
+        position =
+            ( x, y )
+
+        occupation =
+            getOccupation world position
+
+        robot =
+            model.state |> Tuple.first
+
+        robot_position =
+            robot.position
+    in
+        Html.span
+            [ Attribute.classList
+                [ ( "Position", True )
+                , ( toString occupation, True )
+                , ( "Robot-" ++ (toString robot.heading), position == robot_position )
+                ]
+            ]
+            []
 
 
 subscriptions : Model -> Sub msg
